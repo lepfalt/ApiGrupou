e35f77cd1d0ba699179020ab3d9f9db00fd4a62c
leticia.pfaltzgraff <lepfalt@gmail.com>
2020-11-21 20:00:00 -0300

    Modelo de turma.

---
+++ app/db/models/turma.model.js
@@ -0,0 +1,82 @@
+const {DataTypes} = require('sequelize');
+const sequelize = require('../index').getConnection();
+const name = require('path').basename(__filename.replace('.model', ''), '.js')
+
+const Turma = sequelize.define(name, {
+  codigo_identificador: {
+    type: DataTypes.STRING(30),
+    allowNull: false
+  },
+  createdAt: {
+    type: DataTypes.DATE,
+    field: 'criado_em'
+  },
+  updatedAt: {
+    type: DataTypes.DATE,
+    field: 'atualizado_em'
+  }
+}, {
+  sequelize,
+  tableName: name
+});
+
+Turma.associate = (models)=>{
+  Turma.belongsTo(models.disciplina, {
+    foreignKey: {
+      name: 'id_disciplina'
+    },
+    as: 'disciplina'
+  });
+
+  Turma.hasMany(models.grupo, {
+    foreignKey: {
+      name: 'id_turma'
+    },
+    as: 'grupos'
+  });
+
+  Turma.hasMany(models.atividade_avaliativa, {
+    foreignKey: {
+      name: 'id_turma'
+    },
+    as: 'atividades_avaliativas'
+  });
+
+  Turma.belongsToMany(models.curso, {
+    through: 'turma_curso',
+    timestamps: false,
+    foreignKey: {
+      name: 'id_turma',
+    },
+    as: 'cursos'
+  });
+
+  Turma.belongsToMany(models.hardskill, {
+    through: 'turma_hardskill',
+    timestamps: false,
+    foreignKey: {
+      name: 'id_turma',
+    },
+    as: 'hardskills'
+  });
+
+  Turma.belongsToMany(models.aluno, {
+    through: 'turma_aluno',
+    timestamps: false,
+    foreignKey: {
+      name: 'id_turma',
+    },
+    as: 'alunos'
+  });
+
+  Turma.belongsToMany(models.professor, {
+    through: 'turma_professor',
+    timestamps: false,
+    foreignKey: {
+      name: 'id_turma',
+    },
+    as: 'professores'
+  });
+}
+
+module.exports = Turma;
